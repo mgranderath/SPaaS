2bb6a372a0768139022f05176b6436c85797daa9
Malte Granderath <m.granderath@jacobs-university.de>
2018-08-18 15:40:31 +0100

    create application function is done

--- server/controller/app.go
+++ server/controller/app.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -38,12 +39,11 @@ func CreateApplication(c echo.Context) error {
 		Type:    "info",
 		Message: "Creating directories",
 	}); err != nil {
-		return c.JSON(http.StatusConflict, Application{
+		return c.JSON(http.StatusInternalServerError, Application{
 			Type:    "error",
 			Message: err.Error(),
 		})
 	}
-	c.Response().Flush()
 	err := os.MkdirAll(repoPath, os.ModePerm)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Application{
@@ -55,23 +55,23 @@ func CreateApplication(c echo.Context) error {
 		Type:    "success",
 		Message: "Creating directories",
 	}); err != nil {
-		return c.JSON(http.StatusConflict, Application{
+		return c.JSON(http.StatusInternalServerError, Application{
 			Type:    "error",
 			Message: err.Error(),
 		})
 	}
-	// Initlialize the git repository
+	// Initialize the git repository
 	if err := common.EncodeJSONAndFlush(c, Application{
 		Type:    "info",
 		Message: "Creating git repo",
 	}); err != nil {
-		return c.JSON(http.StatusConflict, Application{
+		return c.JSON(http.StatusInternalServerError, Application{
 			Type:    "error",
 			Message: err.Error(),
 		})
 	}
 	if _, err := git.PlainInit(repoPath, true); err != nil {
-		return c.JSON(http.StatusConflict, Application{
+		return c.JSON(http.StatusInternalServerError, Application{
 			Type:    "error",
 			Message: err.Error(),
 		})
@@ -80,10 +80,59 @@ func CreateApplication(c echo.Context) error {
 		Type:    "success",
 		Message: "Creating git repo",
 	}); err != nil {
-		return c.JSON(http.StatusConflict, Application{
+		return c.JSON(http.StatusInternalServerError, Application{
 			Type:    "error",
 			Message: err.Error(),
 		})
 	}
+	// Create git pos-receive hook
+	if err := common.EncodeJSONAndFlush(c, Application{
+		Type:    "info",
+		Message: "Creating git receive hook",
+	}); err != nil {
+		return c.JSON(http.StatusInternalServerError, Application{
+			Type:    "error",
+			Message: err.Error(),
+		})
+	}
+	err = os.MkdirAll(filepath.Join(repoPath, "hooks"), os.ModePerm)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Application{
+			Type:    "error",
+			Message: err.Error(),
+		})
+	}
+	file, err := os.Create(filepath.Join(repoPath, "hooks", "post-receive"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Application{
+			Type:    "error",
+			Message: err.Error(),
+		})
+	}
+	defer file.Close()
+	fmt.Fprintf(file, "#!/usr/bin/env bash\necho \"Starting deploy!\"\n"+
+		"curl --request POST 'http://127.0.0.1:5000/api/v1/app/%s/deploy' |"+
+		"python -c 'import json,sys;obj=json.load(sys.stdin);print \"Successfully deployed!\";print \"Port: \"+obj[\"port\"]'\n", name)
+	// Make the hook executable
+	err = os.Chmod(filepath.Join(repoPath, "hooks", "post-receive"), 0755)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Application{
+			Type:    "error",
+			Message: err.Error(),
+		})
+	}
+	if err := common.EncodeJSONAndFlush(c, Application{
+		Type:    "success",
+		Message: "Creating git receive hook",
+	}); err != nil {
+		return c.JSON(http.StatusInternalServerError, Application{
+			Type:    "error",
+			Message: err.Error(),
+		})
+	}
+	return nil
+}
+
+func getApplications(c echo.Context) error {
 	return nil
 }
