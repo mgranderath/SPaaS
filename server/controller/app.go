e43e9c0bc359e8d2e84d0d7c36bfa8961016c30e
Malte Granderath <m.granderath@jacobs-university.de>
2018-08-20 23:12:47 +0100

    delete goroutine

--- server/controller/app.go
+++ server/controller/app.go
@@ -126,6 +126,36 @@ func create(name string, messages chan<- Application) {
 	close(messages)
 }
 
+func delete(name string, messages chan<- Application) {
+	appPath := filepath.Join(basePath, "applications", name)
+	if !common.Exists(appPath) {
+		messages <- Application{
+			Type:    "error",
+			Message: "Does not exist",
+		}
+		close(messages)
+		return
+	}
+	// Remove directories
+	messages <- Application{
+		Type:    "info",
+		Message: "Removing directories",
+	}
+	if err := os.RemoveAll(appPath); err != nil {
+		messages <- Application{
+			Type:    "error",
+			Message: err.Error(),
+		}
+		close(messages)
+		return
+	}
+	messages <- Application{
+		Type:    "success",
+		Message: "Removing directories",
+	}
+	close(messages)
+}
+
 // CreateApplication creates a new application
 func CreateApplication(c echo.Context) error {
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
@@ -149,37 +179,15 @@ func DeleteApplication(c echo.Context) error {
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	c.Response().WriteHeader(http.StatusOK)
 	name := c.Param("name")
-	appPath := filepath.Join(basePath, "applications", name)
-	if !common.Exists(appPath) {
-		return c.JSON(http.StatusConflict, Application{
-			Type:    "error",
-			Message: "Does not exist",
-		})
-	}
-	// Remove directories
-	if err := common.EncodeJSONAndFlush(c, Application{
-		Type:    "info",
-		Message: "Removing directories",
-	}); err != nil {
-		return c.JSON(http.StatusInternalServerError, Application{
-			Type:    "error",
-			Message: err.Error(),
-		})
-	}
-	if err := os.RemoveAll(appPath); err != nil {
-		return c.JSON(http.StatusConflict, Application{
-			Type:    "error",
-			Message: err.Error(),
-		})
-	}
-	if err := common.EncodeJSONAndFlush(c, Application{
-		Type:    "success",
-		Message: "Removing directories",
-	}); err != nil {
-		return c.JSON(http.StatusInternalServerError, Application{
-			Type:    "error",
-			Message: err.Error(),
-		})
+	messages := make(chan Application)
+	go delete(name, messages)
+	for elem := range messages {
+		if err := common.EncodeJSONAndFlush(c, elem); err != nil {
+			return c.JSON(http.StatusInternalServerError, Application{
+				Type:    "error",
+				Message: err.Error(),
+			})
+		}
 	}
 	return nil
 }
