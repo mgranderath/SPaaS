d21f0063cfa84c09e701d55dca4ed1353b761359
Malte Granderath <m.granderath@jacobs-university.de>
2018-09-04 15:25:43 -0400

    added some tests

--- server/controller/create_test.go
+++ server/controller/create_test.go
@@ -1,12 +1,38 @@
 package controller
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
+	"docker.io/go-docker/api/types"
 	"github.com/magrandera/SPaaS/common"
 	"github.com/magrandera/SPaaS/config"
 )
+
+func testRemoveApp(appPath string, t *testing.T) {
+	if err := os.RemoveAll(appPath); err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func testRemoveContainer(name string, t *testing.T) {
+	err := dock.Cli.ContainerRemove(dock.Ctx, name, types.ContainerRemoveOptions{
+		Force: true,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func testRemoveImage(name string, t *testing.T) {
+	_, err := dock.Cli.ImageRemove(dock.Ctx, name, types.ImageRemoveOptions{
+		Force: true,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
 
 func TestItCreatesNewApp(t *testing.T) {
 	messages := make(chan Application)
@@ -18,10 +44,12 @@ func TestItCreatesNewApp(t *testing.T) {
 	go create(name, messages)
 	for elem := range messages {
 		if elem.Type == "error" {
-			t.Fatal("Gave back an error")
+			testRemoveApp(appPath, t)
+			t.Fatal("Gave back an error: " + elem.Message)
 		}
 	}
 	if !common.Exists(repoPath) {
 		t.Fatal("Did not create the repository")
 	}
+	testRemoveApp(appPath, t)
 }
