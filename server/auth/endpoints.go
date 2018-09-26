dd8a109604a0ceff2460ef0a56fdf2646fdaa394
Malte Granderath <22001111+magrandera@users.noreply.github.com>
2018-09-26 21:53:31 +0100

    simple changes suggested (#39)

--- server/auth/endpoints.go
+++ server/auth/endpoints.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"net/http"
 	"time"
+	"unicode/utf8"
 
 	"github.com/magrandera/SPaaS/common"
 
@@ -14,7 +15,7 @@ import (
 // ChangePassword allows for changing the password
 func ChangePassword(c echo.Context) error {
 	newPassword := c.FormValue("password")
-	if len(newPassword) < 8 {
+	if utf8.RuneCountInString(newPassword) < 8 {
 		return c.JSON(http.StatusBadRequest, map[string]string{
 			"error": "password has to be at leat 8 characters",
 		})
@@ -34,28 +35,28 @@ func ChangePassword(c echo.Context) error {
 func Login(c echo.Context) error {
 	username := c.FormValue("username")
 	password := c.FormValue("password")
-	if username == config.Cfg.Config.GetString("username") && common.CheckPasswordHash(password, config.Cfg.Config.GetString("password")) {
-		// Create token
-		token := jwt.New(jwt.SigningMethodHS256)
-
-		// Set claims
-		claims := token.Claims.(jwt.MapClaims)
-		claims["username"] = username
-		claims["admin"] = true
-		claims["created"] = time.Now().Unix()
-		claims["exp"] = time.Now().Add(time.Hour * 24 * 365).Unix()
-
-		// Generate encoded token and send it as response.
-		t, err := token.SignedString([]byte(config.Cfg.Config.GetString("secret")))
-		if err != nil {
-			return err
-		}
-		return c.JSON(http.StatusOK, map[string]string{
-			"token": t,
-		})
+	if !(username == config.Cfg.Config.GetString("username")) ||
+		!common.CheckPasswordHash(password, config.Cfg.Config.GetString("password")) {
+		return echo.ErrUnauthorized
 	}
 
-	return echo.ErrUnauthorized
+	// Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 24 * 365).Unix()
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte(config.Cfg.Config.GetString("secret")))
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
 }
 
 // GetToken generates a token for internal request use
